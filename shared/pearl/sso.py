f4b0e12a4e237b243b508973a8e0bb7812087d22
Devin Carlen <devin.carlen@gmail.com>
2009-08-22 15:58:17 -0700

    added validate_session

--- shared/pearl/sso.py
+++ shared/pearl/sso.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import User, AnonymousUser
 from django.conf import settings
 import django_pipes as pipes
 
-
+SSO_SESSION_KEY = '_sso_user_id'
 
 def fetch_user(user_id):
     """
@@ -32,22 +32,53 @@ def load_user(user_id):
         user.save()
     return user
 
-# FIXME: session invalidation should not be lazy
+def read_sso_cookie(request):
+    return request.COOKIES['sso'].split('|')[0]
+
+
+def validate_session(request):
+    """
+    handle any sso login changes that should affect the session
+    """
+    
+    try:
+        user_id = read_sso_cookie(request)
+    except:
+        user_id = None
+
+    # If a session has been previously configured
+    if SSO_SESSION_KEY in request.session:
+        # If the user is authenticated but a different user has signed in
+        if user_id:
+            if request.session[SSO_SESSION_KEY] != user_id:
+                request.session.flush()
+                request.session[SSO_SESSION_KEY] = user_id
+        # If the user has signed out and then come back to client site
+        else:
+            request.session.flush()
+    # If not, then configure one.
+    else:
+        request.session_cycle_key()
+        request.session[SSO_SESSION_KEY] = user_id
+
+
 class LazyUser(object):
     def __get__(self, request, obj_type=None):
         if not hasattr(request, '_cached_user'):
             try:
-                user_id = request.COOKIES['sso'].split('|')[0]
+                user_id = read_sso_cookie(request)
                 # FIXME: verify signature
                 user = load_user(user_id)
             except Exception, e:
                 user = AnonymousUser()
-            # FIXME: if session already had an SSO_USER and it is different
-            #        then a new session should be created
+
             request._cached_user = user
         return request._cached_user
 
+# FIXME: rename to SSOAuthenticationMiddleware or similar
 class AuthMiddleware(object):
     def process_request(self, request):
         request.__class__.user = LazyUser()
+
+        validate_session(request)
         return None
